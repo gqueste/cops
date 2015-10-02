26e6834e2a030ac16abd4ea91ddcb50cc0fc0c8c
gqueste <gabriel.queste@gmail.com>
2015-10-02 10:05:21 +0200

    Origine : Gestion de la sélection d'une origine

--- app/scripts/pages/origine/origine_controller.js
+++ app/scripts/pages/origine/origine_controller.js
@@ -4,4 +4,26 @@ angular.module('cops').
 
         $scope.service = OrigineService;
 
+        $scope.activeOrigine = {};
+
+        $scope.changeOrigine = function(origine){
+            $scope.activeOrigine =  origine;
+            $scope.activeOrigine.possessionChoisie = origine.possessionsPossibles[0];
+        };
+
+        $scope.changeOrigine(OrigineService.getOrigines()['enfant_ghetto']);
+
+        $scope.isActive = function(origine){
+            return $scope.activeOrigine.nom == origine.nom;
+        };
+
+        $scope.changePossession = function(possession){
+            $scope.activeOrigine.possessionChoisie = possession;
+        };
+
+        $scope.goToEtudes = function(){
+            OrigineService.setOrigineSelectionnee($scope.activeOrigine);
+            console.log(OrigineService.getOrigineSelectionnee());
+        };
+
     }]);
